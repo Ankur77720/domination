c9751c5c900013876c4f543df91e8ba26659ff41
Ankur77720 <prajapatiankur8349@gmail.com>
2024-09-20 17:19:50 +0530

    path update

--- config/firbse.config.js
+++ config/firbse.config.js
@@ -1,5 +1,5 @@
 const fbAdmin = require("firebase-admin");
-const serviceCredentianls = require("../etc/secret/backend-domination-firebase-adminsdk-xi1is-5a3b954d26.json");
+const serviceCredentianls = require("/etc/secret/backend-domination-firebase-adminsdk-xi1is-5a3b954d26.json");
 
 fbAdmin.initializeApp({
     credential: fbAdmin.credential.cert(serviceCredentianls),
